81fb2cc94ff37b4d79c4dd4390466014be22c0d8
nusislamsheikh472 <nurislamsheikh472@gmail.com>
2022-05-13 10:02:15 +0600

    added these files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let navContainer = document.getElementById("navContainer");
+ 
+function watchBtn() {
+   navContainer.style.right = "0"
+}
+
+function closeMenu() {
+   navContainer.style.right = "-200px"
+}
+
+
+
+
+
